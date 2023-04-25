82da9b28ab1b9e215d859fa7ea4cb17b2ee84a09
n-ikeda <n-ikeda@beingcorp.co.jp>
2023-04-25 17:12:47 +0900

    作成日・更新日を追加。日本時間に変更。

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Tsurizao
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
 
     #ActiveStorageで上書きしない
     config.active_storage.replace_on_assign_to_many = false
