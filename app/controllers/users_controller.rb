612b6077b3c458a675367853c3db24fe5e4dec78
n-ikeda <n-ikeda@beingcorp.co.jp>
2023-04-25 10:47:41 +0900

    ユーザー一覧ページの作成。ユーザーアイコンの表示も可能にした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   def index
+    @users = User.with_attached_avatar.order(:id).page(params[:page])
   end
 
   def show
+    @user = User.find(params[:id])
   end
 end
