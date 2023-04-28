be878440ec672f0c8e62761bc28b0961bd09ada1
n-ikeda <n-ikeda@beingcorp.co.jp>
2023-04-28 11:58:32 +0900

    emailを表示しない。コメントに必要なカラムやモデルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_27_024118) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_28_023849) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -42,6 +42,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_27_024118) do
   create_table "areas", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "comment_content"
+    t.integer "user_id", null: false
+    t.integer "fishlist_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["fishlist_id"], name: "index_comments_on_fishlist_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "fishlists", force: :cascade do |t|
@@ -103,4 +113,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_27_024118) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "fishlists"
+  add_foreign_key "comments", "users"
 end
