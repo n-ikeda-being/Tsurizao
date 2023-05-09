9a0446c388696dad70ca9b580377ecd2b49b33f8
n-ikeda <n-ikeda@beingcorp.co.jp>
2023-05-09 11:54:14 +0900

    デバッグ用の記述削除。アイコンが無い場合はNoImage画像を付ける

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,14 +1,12 @@
 class FollowsController < ApplicationController
     # フォローする
     def create
-        puts 'create action called' # デバッグ出力
         current_user.follow(params[:user_id])
         redirect_to user_path(params[:user_id])
     end
 
     # アンフォロー
     def destroy
-        puts 'destroy action called' # デバッグ出力
         current_user.unfollow(params[:user_id])
         redirect_to user_path(params[:user_id])
     end
