0101dedbe6e500536487eaedcb28d01356e57660
n-ikeda <n-ikeda@beingcorp.co.jp>
2023-05-18 16:45:59 +0900

    写真を6枚までアップ可能に制限する。投稿一覧の並びを横にする。

--- app/models/fishlist.rb
+++ app/models/fishlist.rb
@@ -7,6 +7,14 @@ class Fishlist < ApplicationRecord
     has_many :comments, dependent: :destroy
     has_many :favorites, dependent: :destroy
 
+    validate :validate_image_count
+
+    def validate_image_count
+        if fishimg.attached? && fishimg.count > 6
+            errors.add(:base, message: "写真は6枚まで添付できます")
+        end
+    end
+
     def self.ransackable_attributes(auth_object = nil)
         %w[date prefecture_name prefecture_id place fishname fishingmethod user_name]
     end
