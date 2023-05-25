bfb354c1dbeb5b73ca64874c9619c1e57897431e
n-ikeda <n-ikeda@beingcorp.co.jp>
2023-05-25 16:06:48 +0900

    ページネーションの数字を変える

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.with_attached_avatar.order(:id).page(params[:page]).per(10)
+    @users = User.with_attached_avatar.order(:id).page(params[:page]).per(20)
     @q = User.ransack(params[:q])
     @user = @q.result
   end
